7efcddccc8ca9dd70b3b40c50b51c65530bfd4f6
Markus Meissner <markus.meissner@gmx.de>
2023-11-09 16:59:01 +0100

    added docker command

--- functions_download_dwd.sh
+++ functions_download_dwd.sh
@@ -24,9 +24,14 @@ download_merge_2d_variable_icon_d2()
 	filename="icon-d2_germany_regular-lat-lon_single-level_${year}${month}${day}${run}_*_2d_${1}.grib2"
 	filename_grep="icon-d2_germany_regular-lat-lon_single-level_${year}${month}${day}${run}_(.*)_2d_${1}.grib2.bz2"
 	url="https://opendata.dwd.de/weather/nwp/icon-d2/grib/${run}/${1}/"
+	forecast_hours=
 	if [ ! -f "${1}_${year}${month}${day}${run}_de.nc" ]; then
 		listurls $filename_grep $url | parallel -j 10 get_and_extract_one {}
-		cdo -f nc copy -seltime,00:00,01:00,02:00,03:00,04:00,05:00,06:00,07:00,08:00,09:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00 -mergetime ${filename} ${1}_${year}${month}${day}${run}_de.nc
+		docker run --rm --volume .:/data  deutscherwetterdienst/regrid:icon-d2 \
+			cdo --reduce_dim -f nc copy \
+				-seltime,00:00,01:00,02:00,03:00,04:00,05:00,06:00,07:00,08:00,09:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00 \
+				-mergetime \
+				/data/${filename} /data/${1}_${year}${month}${day}${run}_de.nc
 		rm ${filename}
 	fi
 }
@@ -52,7 +57,8 @@ download_invariant_icon_d2()
 	filename="icon-d2_germany_regular-lat-lon_time-invariant_${year}${month}${day}${run}_000_0_hsurf.grib2"
 	wget -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon-d2/grib/${run}/hsurf/"
 	bzip2 -d ${filename}.bz2 
-	cdo -f nc copy ${filename} HSURF_${year}${month}${day}${run}_de.nc
+	docker run --rm --volume ./data:/data    deutscherwetterdienst/regrid:icon-d2 \
+		cdo -f nc copy /data/${filename} /data/HSURF_${year}${month}${day}${run}_de.nc
 	rm ${filename}
 }
 export -f download_invariant_icon_d2
